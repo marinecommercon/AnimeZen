b69ec9a775e78ef4c1df1617ca0b26bad6efa230
Marine Commerçon <Technique@Marine-Comm.local>
2016-06-18 09:03:32 +0200

    first commit

---
+++ MyAnimationExercise/MyAnimationExercise/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyAnimationExercise
+//
+//  Created by Technique on 18/06/2016.
+//  Copyright © 2016 Marine Commerçon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
